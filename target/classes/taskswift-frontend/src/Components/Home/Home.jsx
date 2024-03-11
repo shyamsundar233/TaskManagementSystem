3fc21500b7c9a02ec7728147973c29d2de59d62a
shyamsundar233 <shyamvj233@gmail.com>
2024-03-11 12:51:28 +0530

    Logout button in home page

--- target/classes/taskswift-frontend/src/Components/Home/Home.jsx
+++ target/classes/taskswift-frontend/src/Components/Home/Home.jsx
@@ -2,14 +2,25 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 
 import React from 'react'
+import {Button} from "@mui/material";
+import axios from "axios";
 
 const Home = () => {
+
+    const handleLogout = () =>{
+        axios.post("/logout").then(resp => {
+            if(resp.status === 200){
+                window.location.href = "/login";
+            }
+        })
+    }
+
   return (
       <div>
           <Link to="/ts/create">Create</Link><br/>
           <Link to="/ts/list">List</Link><br/>
           <Link to="/ts/addUser">Add User</Link><br/>
-          <Link to="/logout">Logout</Link>
+          <Button to="/logout" onClick={handleLogout}>Logout</Button>
       </div>
   )
 }
