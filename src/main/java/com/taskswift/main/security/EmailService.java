bf4d17c6fefb7c5961316046ae198b5a33f85227
shyamsundar233 <shyamvj233@gmail.com>
2024-04-05 05:59:34 +0530

    Email Service Implementation

---
+++ src/main/java/com/taskswift/main/security/EmailService.java
@@ -0,0 +1,33 @@
+package com.taskswift.main.security;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromMail;
+
+    @Async
+    public void sendMail(String to, String subject, String body) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+
+        mimeMessageHelper.setFrom(fromMail);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(body, false);
+
+        mailSender.send(mimeMessage);
+    }
+}
