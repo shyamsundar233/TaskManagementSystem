af9a9ff419e12837136acfb5bb24c35b3f7edede
shyamsundar233 <shyamvj233@gmail.com>
2024-04-16 07:06:02 +0530

    Home page full design

--- src/main/resources/taskswift-frontend/src/Components/Home/Home.jsx
+++ src/main/resources/taskswift-frontend/src/Components/Home/Home.jsx
@@ -5,6 +5,7 @@ import StatsComponent from "../StatsComponent/StatsComponent";
 import TableComponent from "../TableComponent/TableComponent";
 import axios from "axios";
 import {Link} from "react-router-dom";
+import { PieChart } from '@mui/x-charts/PieChart';
 
 const constructGraphForTask = (tasksList) => {
     tasksList.sort((task1, task2) => {
@@ -38,6 +39,7 @@ const Home = () => {
     const [weeklyTasks, setWeeklyTasks] = useState([]);
     const [highPriorityTasksCount, setHighPriorityTasksCount] = useState(0);
     const [highPriorityTasks, setHighPriorityTasks] = useState([]);
+    const [usersList, setUsersList] = useState([]);
 
     useEffect(() => {
 
@@ -62,6 +64,10 @@ const Home = () => {
                 setHighPriorityTasks(graphArr)
             }
         })
+
+        axios.get("/v1/api/users").then(resp => {
+            constructUsersList(resp.data.Users)
+        })
     }, []);
 
     const constructTodayTask = (tasksList) => {
@@ -79,19 +85,27 @@ const Home = () => {
         setTodayTask(tasksArr);
     }
 
+    const constructUsersList = (usersList) => {
+        let usersArr = [];
+        for (let index in usersList) {
+            usersArr.push([usersList[index].username]);
+        }
+        setUsersList(usersArr);
+    }
+
   return (
-      <div className="">
-          <div className="display-center-col scroll-div">
-              <div className="height-width-auto">
-                  <div className="title-font padd-20 margin-top-left-20 font-heading">Statistics</div>
+      <div className="display-center">
+          <div className="display-center-col scroll-div home-section-1">
+              <div className="height-width-auto stats-cont">
+                  <div className="title-font font-heading padd-20">Statistics</div>
                   <div className="display-center home-graph-cont-1">
                       <StatsComponent title="To Do Tasks" value={toDoTasksCount} graphData={toDoTask} className="home-stats-comp-col-1"/>
                       <StatsComponent title="This Week Tasks" value={weeklyTasksCount} graphData={weeklyTasks} className="home-stats-comp-col-2"/>
                       <StatsComponent title="High Priorities" value={highPriorityTasksCount} graphData={highPriorityTasks} className="home-stats-comp-col-3"/>
                   </div>
               </div>
-              <div className="height-width-auto">
-                  <div className="title-font padd-20 margin-top-left-20 font-heading">
+              <div className="height-width-auto task-cont">
+                  <div className="title-font font-heading">
                       <div className="padd-20"> My Today's Tasks </div>
                       {todayTask.length > 0 ?
                           <TableComponent
@@ -103,6 +117,42 @@ const Home = () => {
                           <div className="padd-20 font-sub-heading display-center">No Tasks today !!!</div>
                       }
                   </div>
+              </div>
+          </div>
+          <div className="home-section-2">
+              <div className="users-list-cont">
+                  <div className="title-font font-heading padd-20">Team Members</div>
+                  {usersList.length > 0 ?
+                      <TableComponent
+                          headerRow={["Username"]}
+                          bodyRow={usersList}
+                          classList="height-width-auto scroll-div home-users-tab-dim"
+                      />
+                      :
+                      <div className="padd-20 font-sub-heading display-center">No team members !!!</div>
+                  }
+              </div>
+              <div className="leading-list-cont">
+                  <div className="title-font font-heading padd-20">Leading Contributors</div>
+                  <PieChart
+                      series={[
+                          {
+                              data: [
+                                  { id: 0, value: 20, label: 'Shyam' },
+                                  { id: 1, value: 40, label: 'Archana' },
+                                  { id: 2, value: 15, label: 'Dinesh' },
+                                  { id: 3, value: 20, label: 'Kumar' },
+                                  { id: 4, value: 5, label: 'Nakul' },
+                              ],
+                              innerRadius: 20,
+                              outerRadius: 100,
+                              paddingAngle: 5,
+                              cornerRadius: 5
+                          },
+                      ]}
+                      width={400}
+                      height={200}
+                  />
               </div>
           </div>
       </div>
