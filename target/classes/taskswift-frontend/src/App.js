8170bf7c7c6768557ac3c2f653a1d68cc5567d78
shyamsundar233 <shyamvj233@gmail.com>
2024-04-19 16:00:22 +0530

    App responsive - 1

--- target/classes/taskswift-frontend/src/App.js
+++ target/classes/taskswift-frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Title from './Components/Title/Title';
 import CustomAlert from './Components/CustomAlert/CustomAlert';
 import SideBar from "./Components/SideBar/SideBar";
+import TitleBar from "./Components/TitleBar/TitleBar";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
       <div className="app-outer-div">
-          <Title/>
-          <SideBar/>
+          <TitleBar/>
+          <NavBar/>
           <CustomAlert>
               <div className="outlet-div scroll-div">
                   <Outlet/>
